da70f5108a4e08657a34dc965755e230f565b5fa
Juanito Fatas <katehuang0320@gmail.com>
2020-03-25 14:25:50 +0900

    Add more test cases
    1) ruby markup chain with link
    2) ruby markup annotates separately
    3) ruby markup annotates and links separately
    4) ruby markup annotates separately but links together

--- spec/ruby_markup_spec.rb
+++ spec/ruby_markup_spec.rb
@@ -22,6 +22,39 @@ RSpec.describe RubyMarkup::Filter do
     expect(result).to eq "<ruby>漢字<rp>(</rp><rt>かんじ</rt><rp>)</rp></ruby>"
   end
 
+  it "works with markdown link syntax" do
+    input = "[漢字(かんじ)](https://jisho.org/search/漢字)"
+
+    result = RubyMarkup::Filter.new(input).call
+
+    expect(result).to eq %(<ruby><a href="https://jisho.org/search/漢字" target="_blank" rel="noopener noreferrer" itemprop="url" aria-label="search 漢字 on jisho.org">漢字</a><rt>かんじ</rt></ruby>)
+  end
+
+  it "works when annotates two characters separately" do
+    input = "[漢(かん)][字(じ)]"
+
+    result = RubyMarkup::Filter.new(input).call
+
+    expect(result).to eq "<ruby>漢<rp>(</rp><rt>かん</rt><rp>)</rp></ruby><ruby>字<rp>(</rp><rt>じ</rt><rp>)</rp></ruby>"
+  end
+
+  it "works when annotates and links separately" do
+    input = "[漢(かん)](https://jisho.org/search/漢)[字(じ)](https://jisho.org/search/字)"
+
+    result = RubyMarkup::Filter.new(input).call
+
+    expect(result).to eq %(<ruby><a href="https://jisho.org/search/漢" target="_blank" rel="noopener noreferrer" itemprop="url" aria-label="search 漢 on jisho.org">漢</a><rt>かん</rt></ruby><ruby><a href="https://jisho.org/search/字" target="_blank" rel="noopener noreferrer" itemprop="url" aria-label="search 字 on jisho.org">字</a><rt>じ</rt></ruby>)
+  end
+
+  it "works when annotates two characters separately but links together" do
+    input = "[[漢(かん)][字(じ)]](https://jisho.org/search/漢字)"
+
+    result = RubyMarkup::Filter.new(input).call
+
+    # MarkdownFilter comes after will convert [...](...) into link
+    expect(result).to eq "[<ruby>漢<rp>(</rp><rt>かん</rt><rp>)</rp></ruby><ruby>字<rp>(</rp><rt>じ</rt><rp>)</rp></ruby>](https://jisho.org/search/漢字)"
+  end
+
   it "does not translate wrong ruby markup syntax (no space before left paren)" do
     input = "[漢字  (かんじ)]"
 
